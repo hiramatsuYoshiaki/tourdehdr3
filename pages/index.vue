9693b26cb18af81558777af3122744e11ecebdc4
hiramatsu <hiramatsu3300@gmail.com>
2020-10-01 11:25:25 +0900

    test mobile widht overflow

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,52 @@
 <template lang="pug">
 //- v-container.red.lighten-5 
 v-container.grey.lighten-5 
-  //- section.SC1 
-  //-   h1 section1
+  section.SC1 
+    h1 section1
 
-  v-carousel(
-    cycle,
-    width='100%',
-    height='600',
-    hide-delimiter-background,
-    hide-delimiters,
-    show-arrows,
-    show-arrows-on-hover
-  )
-    v-carousel-item(
-      v-for='(post, i) in posts',
-      :key='i',
-      :src='post.image.url',
-      reverse-transition='fade-transition',
-      transition='fade-transition'
-    )
-  .mt-8 
-    h1 Post Time Line
+  //- v-carousel(
+  //-   cycle,
+  //-   width='100%',
+  //-   height='600',
+  //-   hide-delimiter-background,
+  //-   hide-delimiters,
+  //-   show-arrows,
+  //-   show-arrows-on-hover
+  //- )
+  //-   v-carousel-item(
+  //-     v-for='(post, i) in posts',
+  //-     :key='i',
+  //-     :src='post.image.url',
+  //-     reverse-transition='fade-transition',
+  //-     transition='fade-transition'
+  //-   )
+  //- .mt-8 
+  //-   h1 Post Time Line
+
   //- v-timeline(dense)
-  v-timeline
-    v-timeline-item(v-for='(post, i) in posts', :key='i', small)
-      template(v-slot:opposite)
-        span(
-          :class='`headline font-weight-bold `',
-          v-text='post.stages.stageNo'
-        )
-      .py-4
-        h1(:class='`headline font-weight-light mb-4 `') {{ post.stages.stageNo }}
-        h2(:class='`headline font-weight-light mb-4 `') {{ post.title }}
-        div {{ post.body }}
-        h3 {{ post.link }}
-        h3 {{ post.users.name }}
-        h3 {{ post.locations.locations }}
-        h3 {{ post.stages.stage }}
-        h3 {{ post.stages.title }}
-        div(v-for='tag in post.tags', :key='tag.id')
-          span.mr-4 {{ tag.tags }}
-        img(:src='post.image.url', width='300', height='200', alt='画像') 
-        div 
-          nuxt-link(:to='`/posts/${post.id}`')
-            h3 この投稿をみる
-  v-divider
+  //- v-timeline
+  //-   v-timeline-item(v-for='(post, i) in posts', :key='i', small)
+  //-     template(v-slot:opposite)
+  //-       span(
+  //-         :class='`headline font-weight-bold `',
+  //-         v-text='post.stages.stageNo'
+  //-       )
+  //-     .py-4
+  //-       h1(:class='`headline font-weight-light mb-4 `') {{ post.stages.stageNo }}
+  //-       h2(:class='`headline font-weight-light mb-4 `') {{ post.title }}
+  //-       div {{ post.body }}
+  //-       h3 {{ post.link }}
+  //-       h3 {{ post.users.name }}
+  //-       h3 {{ post.locations.locations }}
+  //-       h3 {{ post.stages.stage }}
+  //-       h3 {{ post.stages.title }}
+  //-       div(v-for='tag in post.tags', :key='tag.id')
+  //-         span.mr-4 {{ tag.tags }}
+  //-       img(:src='post.image.url', width='300', height='200', alt='画像') 
+  //-       div 
+  //-         nuxt-link(:to='`/posts/${post.id}`')
+  //-           h3 この投稿をみる
+  //- v-divider
 </template>
 <script>
 // import axios from 'axios'
